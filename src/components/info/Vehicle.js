07d34e56fbb984a3caef23226b92fcf9f736ce65
lewibs <benjaminsl2000@gmail.com>
2022-05-24 22:35:53 -0400

    started work on editables

--- src/components/info/Vehicle.js
+++ src/components/info/Vehicle.js
@@ -1,35 +1,45 @@
-import {BottomButton, EditImage, Input, Image} from "../oui";
+import {EditImage, Input, Image} from "../oui";
+import Editable from "./Editable";
 import { largeProfile } from "../oui/styles/image";
 import {useEffect, useState, useContext} from 'react';
 import authContext from "../../auth/authContext";
+import Url from "../../classes/url";
 
 const alt = "profile picture";
 
 export default function Vehicle() {
-  const [src, useSrc] = useState("pfp");
   const [car, setCar] = useState({});
+
   const {client} = useContext(authContext)
 
-
   useEffect(()=>{
-  },[])
+    //get the id from the url id query
+    let id = new Url(window.location.href).id;
+    client.vehicles.get(id, (res)=>{
+      //remove all the fields that start with "_"
+      alert("make a vehicle class that has method for returning editable fields");
+      let car = res.data.vehicle;
+      for(let key in car) {
+        if(key.startsWith("_")) {
+          delete car[key];
+        }
+      }
+      setCar(res.data.vehicle);
+    }, (err)=>{
+      console.error(err);
+    })
+  }, []);
 
-  let content = 
-    <div>
-      <Image src={'pfp'} alt={alt} height={largeProfile} />
-      {Object.keys(car).map((key, i)=>{
-        return <Input type={'text'} value={car[key]} onChange={(e)=>setCar((p)=>{
-          return {
-            ...p, 
-            key: e.target.value
-          }
-        })}/>
-      })}
-
-
-
-      <BottomButton icon={"edit"} />
-    </div>
-
-  return content;
+  return(
+    <Editable>
+        {Object.keys(car).map((key, i)=>{
+          return <Input type={'text'} value={car[key]} onChange={(e)=>setCar((p)=>{
+            return {
+              ...p, 
+              key: e.target.value
+            }
+          })}/>
+        })}
+    </Editable>
+  );
 }
