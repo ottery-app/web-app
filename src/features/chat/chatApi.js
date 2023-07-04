f75b566acb43fe1736624ba9a4e81c027ec5bd31
lewibs <benjaminsl2000@gmail.com>
2023-07-03 22:24:03 -0400

    [matinence] updated chatApi to use clide

--- src/features/chat/chatApi.js
+++ src/features/chat/chatApi.js
@@ -1,8 +1,8 @@
 import { axiosInst } from "../../app/axiosInst";
 import { MakeChatDto, classifyWithDto, isId } from "ottery-dto";
 import { ERR_USER } from "../../app/axiosInst";
-import { isString } from "ducktyper";
 import { clideInst } from "../../app/clideInst";
+import { DummyCache } from "../../ottery-cache/DummyCache";
 
 
 
@@ -20,45 +20,40 @@ export const getChatsFor = clideInst
         }
     });
 
-export async function getChat(chatId) {
-    try {
-        isId(chatId, {throw:true});
-    } catch (e) {
-        throw {
-            code: ERR_USER,
-            message: e.message,
-        };
-    }
+export const getChat = clideInst
+    .makeGet("message/chat/:chatId", {
+        param_validators: {
+            chatId: isId,
+        },
+        pipeline: (chatId) => {
+            return {
+                params: {
+                    chatId: chatId,
+                }
+            }
+        }
+    })
 
-    return await axiosInst.get(`api/message/chat/${chatId}`);
-}
+export const sendMessage = clideInst
+    .makePatch("message/chat/direct:chatId", {
+        param_validators: {
+            chatId: isId,
+        },
+        pipeline: (chatId) => {
+            return {
+                params: {
+                    chatId: chatId,
+                }
+            }
+        }
+    })
 
-export async function sendMessage(chatId, message) {
-    try {
-        isString(message, {throw:true});
-    } catch (e) {
-        throw {
-            code: ERR_USER,
-            message: e.message,
-        };
-    }
-
-    return await axiosInst.patch(`api/message/chat/direct/${chatId}`, {string:message});
-}
-
-export async function makeChat(makeChatDto) {
-    try {
-        classifyWithDto(
-            MakeChatDto,
-            makeChatDto,
-            {throw: true}
-        )
-    } catch (e) {
-        throw {
-            code: ERR_USER,
-            message: e.message,
-        };
-    }
-
-    return await axiosInst.put(`api/message/chat`, makeChatDto)
-}
+export const makeChat = clideInst
+    .makePut("message/chat", {
+        data_validator: MakeChatDto,
+        pipeline: (makeChatDto) => {
+            return {
+                data: makeChatDto
+            }
+        }
+    });
