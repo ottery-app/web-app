0484e2849896c1bad37e1119be06a20c4b71e4dd
lewibs <benjaminsl2000@gmail.com>
2023-10-05 22:17:07 -0400

    moved snackbar to top and also removeed double snack issue

--- src/ottery-ping/index.js
+++ src/ottery-ping/index.js
@@ -1,16 +1,14 @@
 import Snackbar from '@mui/material/Snackbar';
-import { createContext, useCallback, useContext, useState } from "react";
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
+import { createContext, useCallback, forwardRef, useContext, useState } from "react";
+//import Alert from '@mui/material/Alert';
+import MuiAlert from '@mui/material/Alert';
 
-//import MuiAlert from '@mui/material/Alert';
-
-// const Alert = forwardRef(function Alert(
-//   props,
-//   ref,
-// ) {
-//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-// });
+const Alert = forwardRef(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const PingContext = createContext();
 
@@ -24,64 +22,66 @@ export const AlertColor = {
 const BASE_OPTIONS = {
     open: false,
     message: "Default message",
-    autoHideDuration: 1500,
+    autoHideDuration: 4000,
     severity: AlertColor.info,
 }
 
 export function PingProvider({children}) {
     const [options, setOptions] = useState(BASE_OPTIONS);
 
-    const success = useCallback((message)=>{
+    const success = useCallback((message) => {
         console.log(message);
-       
-        setOptions({
-            ...BASE_OPTIONS,
+    
+        setOptions((prevOptions) => ({
+            ...prevOptions,
             open: true,
             message,
             severity: AlertColor.success,
-        });
+        }));
     }, []);
-
-    const info = useCallback((message)=>{
+    
+    const info = useCallback((message) => {
         console.log(message);
-
-        setOptions({
-            ...BASE_OPTIONS,
+    
+        setOptions((prevOptions) => ({
+            ...prevOptions,
             open: true,
             message,
-            severity: AlertColor.success,
-        });
+            severity: AlertColor.info, // Change to info
+        }));
     }, []);
-
-    const warning = useCallback((message)=>{
+    
+    const warning = useCallback((message) => {
         console.warn(message);
-
-        setOptions({
-            ...BASE_OPTIONS,
+    
+        setOptions((prevOptions) => ({
+            ...prevOptions,
             open: true,
             message,
             severity: AlertColor.warning,
-        });
+        }));
     }, []);
-
-
-    const error = useCallback((message)=>{
+    
+    const error = useCallback((message) => {
         console.error(message);
-
-        setOptions({
-            ...BASE_OPTIONS,
+    
+        setOptions((prevOptions) => ({
+            ...prevOptions,
             open: true,
             message,
             severity: AlertColor.error,
-        });
+        }));
     }, []);
+    
 
     const handleClose = useCallback((event, reason)=>{
         if (reason === 'clickaway') {
             return;
         }
       
-        setOptions({...options, open:false});
+        setOptions((options)=>{
+            return {...options, open:false}
+        });
     }, []);
 
     return (
@@ -93,14 +93,17 @@ export function PingProvider({children}) {
                 info,
             }}
         >
-            {children}
-            <Snackbar open={options.open} autoHideDuration={options.autoHideDuration} onClose={handleClose}>
-                <Stack sx={{ width: '100%' }} spacing={2}>
-                    <Alert onClose={handleClose} severity={options.severity} sx={{ width: '100%' }}>
-                        {options.message}
-                    </Alert>
-                </Stack>
+            <Snackbar 
+                anchorOrigin={{horizontal:"left", vertical:"top"}} 
+                open={options.open} 
+                autoHideDuration={options.autoHideDuration}
+                onClose={handleClose}
+            >
+                <Alert onClose={handleClose} severity={options.severity} sx={{ width: '100%' }}>
+                    {options.message}
+                </Alert>
             </Snackbar>
+            {children}
         </PingContext.Provider>
     );
 }
