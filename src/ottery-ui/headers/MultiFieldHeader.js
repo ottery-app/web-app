e0d7e73a1c6ee81e5aee7e60b4a9290eb0673c58
Nikunj goyal <40403668+Nike682631@users.noreply.github.com>
2023-10-12 01:39:08 +0530

    refactor: formatted files

--- src/ottery-ui/headers/MultiFieldHeader.js
+++ src/ottery-ui/headers/MultiFieldHeader.js
@@ -1,8 +1,8 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import TabButtons from "../buttons/TabButtons";
 import Image from "../images/Image";
-import { image } from "../styles/image"
+import { image } from "../styles/image";
 import { colors } from "../styles/colors";
 import { radius as rad } from "../styles/radius";
 import { margin } from "../styles/margin";
@@ -13,109 +13,108 @@ import { TabButtonTypes } from "../buttons/tabs/TabButton";
 const IMAGE_RAD = image.mediumProfile;
 
 const Header = styled.div`
+  display: flex;
+  flex-direction: column;
 
-    display: flex;
-    flex-direction: column;
-    
-    gap: ${margin.medium};
-    width: 100%;
-    background-color: ${colors.background.primary};
-    border-radius: ${props=>props.radius} ${props=>props.radius} 0 0;
+  gap: ${margin.medium};
+  width: 100%;
+  background-color: ${colors.background.primary};
+  border-radius: ${(props) => props.radius} ${(props) => props.radius} 0 0;
 `;
 
 const Top = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    width: 100%
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
 `;
 
 const UserDetails = styled.div`
-    display: flex;
-    width: 100%;
-    align-items: start;
-    justify-content: center;
-    flex-direction: row;
-    gap: ${margin.large};
-    margin: ${margin.medium};
+  display: flex;
+  width: 100%;
+  align-items: start;
+  justify-content: center;
+  flex-direction: row;
+  gap: ${margin.large};
+  margin: ${margin.medium};
 `;
 
 const Title = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 const Settings = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: end;
-    margin-top: ${margin.medium};
-`
+  display: flex;
+  width: 100%;
+  justify-content: end;
+  margin-top: ${margin.medium};
+`;
 
 const MarginRight = styled.div`
-    margin-right: ${margin.large}
-`
+  margin-right: ${margin.large};
+`;
 
 export function MultiFieldHeader({
-    //top row
-    title="title", //can be an array
+  //top row
+  title = "title", //can be an array
 
-    //image
-    src="pfp",
-    alt="profile photo",
+  //image
+  src = "pfp",
+  alt = "profile photo",
 
-    //tabs
-    tabs=[],
-    tab=tabs[0],
-    onTab=()=>{},
+  //tabs
+  tabs = [],
+  tab = tabs[0],
+  onTab = () => {},
 
-    settings=null,
+  settings = null,
 
-    //style
-    radius=rad.square,
+  //style
+  radius = rad.square,
 }) {
-    const head = (Array.isArray(title)) ? title : [title];
-    const name = head[0].split(" ")
+  const head = Array.isArray(title) ? title : [title];
+  const name = head[0].split(" ");
 
-    //color=useColors({color});
+  //color=useColors({color});
 
-    return (
-        <>
-        <Header
-            radius={radius}
-        >
-            <Top>
+  return (
+    <>
+      <Header radius={radius}>
+        <Top>
+          <UserDetails>
+            <Image
+              src={src}
+              alt={alt}
+              width={IMAGE_RAD}
+              height={IMAGE_RAD}
+              radius={rad.round}
+            />
+            <Title>
+              {name[0].charAt(0).toUpperCase() + name[0].slice(1)}
+              <br />
+              {name.splice(1)}
+            </Title>
+          </UserDetails>
 
-                <UserDetails>
+          {settings && (
+            <Settings>
+              <MarginRight>
                 <Image
-                    src={src}
-                    alt={alt}
-                    width={IMAGE_RAD}
-                    height={IMAGE_RAD}
-                    radius={rad.round} />
-                    <Title>
-                    {name[0].charAt(0).toUpperCase() + name[0].slice(1)}
-                    <br/>
-                    {name.splice(1)}
-                    </Title>
-                </UserDetails>
-
-                {settings && 
-                <Settings>
-                    <MarginRight>
-                    <Image
-                        src="gear"
-                        alt={alt}
-                        width={image.extraSmallProfile}
-                        height={image.extraSmallProfile}
-                        radius={rad.round} />
-                    </MarginRight>
-                    </Settings>
-                }
-            </Top>
-                <TabField
-                    type={TabButtonTypes.upright}
-                    tabs={tabs}
-                    active={tab}
-                    onTab={onTab} />
-        </Header>
-            </>
-    );
+                  src="gear"
+                  alt={alt}
+                  width={image.extraSmallProfile}
+                  height={image.extraSmallProfile}
+                  radius={rad.round}
+                />
+              </MarginRight>
+            </Settings>
+          )}
+        </Top>
+        <TabField
+          type={TabButtonTypes.upright}
+          tabs={tabs}
+          active={tab}
+          onTab={onTab}
+        />
+      </Header>
+    </>
+  );
 }
