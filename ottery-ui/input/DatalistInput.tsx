a85956113b735be3d03240929eeb3e4c9df00227
Nikunj goyal <40403668+Nike682631@users.noreply.github.com>
2023-11-12 23:40:16 +0530

    refactored : dropdown input

--- ottery-ui/input/DatalistInput.tsx
+++ ottery-ui/input/DatalistInput.tsx
@@ -1,94 +1,98 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
-import { colors } from '../styles/colors';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { colors } from "../styles/colors";
+import { border } from "../styles/border";
+import { radius } from "../styles/radius";
+import { margin } from "../styles/margin";
+import { clickable } from "../styles/clickable";
+import { font } from "../styles/font";
 
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      padding: 16,
-    },
-    dropdown: {
-      height: 50,
-      borderColor: colors.primary.main,
-      borderWidth: 0.5,
-      borderRadius: 8,
-      paddingHorizontal: 8,
-    },
-    icon: {
-      marginRight: 5,
-    },
-    label: {
-      position: 'absolute',
-      backgroundColor: 'white',
-      left: 22,
-      top: 8,
-      zIndex: 999,
-      paddingHorizontal: 8,
-      fontSize: 14,
-    },
-    placeholderStyle: {
-      fontSize: 16,
-      color: colors.background.contrast
-    },
-    selectedTextStyle: {
-      fontSize: 16,
-    },
-    iconStyle: {
-      width: 20,
-      height: 20,
-    },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-    },
-  });
-//Instead of defining the value as key in the dropdownoption interface we are defining it as label 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 1.6 * radius.default,
+  },
+  dropdown: {
+    height: 5 * radius.default,
+    borderColor: colors.primary.main,
+    borderWidth: 0.5 * border.thin,
+    borderRadius: 0.8 * radius.default,
+    paddingHorizontal: 0.8 * radius.default,
+  },
+  icon: {
+    marginRight: margin.small,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: colors.background.primary,
+    left: 2.2 * radius.default,
+    top: 0.8 * radius.default,
+    zIndex: 999,
+    paddingHorizontal: 0.8 * radius.default,
+    fontSize: 1.4 * radius.default,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    color: colors.background.contrast,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: margin.large,
+    height: margin.large,
+  },
+});
+//Instead of defining the value as key in the dropdownoption interface we are defining it as label
 //because the native dropdoen component are expection values as options/data as label value pairs
-  interface DropdownOption {
-    label: string;
-    value: string;
-  }
+interface DropdownOption {
+  label: string;
+  value: string;
+}
 
-  interface DropdownProps {
-    label: string;
-    options: DropdownOption[];
-    value: string;
-    placeholder: string;
-    onChange: (key: string) => void
-  }
-  const DatalistInput = ({label, options, onChange, placeholder, value}: DropdownProps) => {
-    const renderLabel = () => {
-        return (
-          <Text style={[styles.label,{ color: colors.text.primary }]}>
-            {label}
-          </Text>
-        );
-    };
-
+interface DropdownProps {
+  label: string;
+  options: DropdownOption[];
+  value: string;
+  placeholder: string;
+  onChange: (key: string) => void;
+}
+const DatalistInput = ({
+  label,
+  options,
+  onChange,
+  placeholder,
+  value,
+}: DropdownProps) => {
+  const renderLabel = () => {
     return (
-      <View style={styles.container}>
-        {renderLabel()}
-        <Dropdown
-          style={[styles.dropdown,{ borderColor: colors.primary.main }]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={options}
-          search
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          placeholder={placeholder}
-          searchPlaceholder="Search"
-          value={value}
-          onChange={item => {
-            onChange(item.value);
-          }}
-        />
-      </View>
+      <Text style={[styles.label, { color: colors.text.primary }]}>
+        {label}
+      </Text>
     );
   };
 
-  export default DatalistInput;
+  return (
+    <View style={styles.container}>
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, { borderColor: colors.primary.main }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        iconStyle={styles.iconStyle}
+        data={options}
+        maxHeight={2 * clickable.maxHeight}
+        labelField="label"
+        valueField="value"
+        placeholder={placeholder}
+        value={value}
+        onChange={(item) => {
+          onChange(item.value);
+        }}
+      />
+    </View>
+  );
+};
+
+export default DatalistInput;
