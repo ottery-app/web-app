04e7a47255eaa5752a156068f013c0fec08ff239
Nikunj goyal <40403668+Nike682631@users.noreply.github.com>
2023-11-13 01:04:29 +0530

    refactored : phone input added validator and error message

--- ottery-ui/input/PhoneInput.tsx
+++ ottery-ui/input/PhoneInput.tsx
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { TextInput } from 'react-native-paper';
-import { colors } from '../styles/colors';
-import { margin } from '../styles/margin';
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+// import { TextInput } from "react-native-paper";
+import { colors } from "../styles/colors";
+import { margin } from "../styles/margin";
+import { radius } from "../styles/radius";
+import TextInput from "./TextInput";
 
+const PhoneNumberInput = ({ label, value, onChange }) => {
+  const validator = () => {
+    const phoneRegex =
+      /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$|^$/;
 
-const PhoneNumberInput = ({label,value,onChange}) => {
-  const [validationText, setValidationText] = useState('');
-  useEffect(() => {
-    const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$|^$/;
-
-    if (phoneRegex.test(value)) {
-      setValidationText('');
-    } else {
-      setValidationText('Please enter a valid phone number!');
-    }
-  },[value])
+    return phoneRegex.test(value);
+  };
 
   return (
     <View>
       <TextInput
-        mode='outlined'
         label={label}
-        outlineStyle={{ borderRadius: 10 }}
+        mode="outlined"
+        outlineStyle={{ borderRadius: radius.default }}
+        outlineColor={colors.primary.main}
+        placeholder="Phone number"
+        placeholderTextColor={colors.background.contrast}
+        onChange={onChange}
+        keyboardType="numeric"
+        validator={validator}
+        value={value}
+        errorMsg={"Please provide a valid number"}
+      />
+      {/* <TextInput
+        mode="outlined"
+        label={label}
+        outlineStyle={{ borderRadius: radius.default }}
         outlineColor={colors.primary.main}
         placeholder="Phone number"
         placeholderTextColor={colors.background.contrast}
@@ -30,7 +40,16 @@ const PhoneNumberInput = ({label,value,onChange}) => {
         onChangeText={(text) => onChange(text)}
         value={value}
       />
-      <Text style={{marginLeft: margin.small, marginTop: margin.small, fontSize: 12, color: colors.error.main}} >{validationText}</Text>
+      <Text
+        style={{
+          marginLeft: margin.small,
+          marginTop: margin.small,
+          fontSize: 12,
+          color: colors.error.main,
+        }}
+      >
+        {validationText}
+      </Text> */}
     </View>
   );
 };
--- ottery-ui/input/TextInput.js
+++ ottery-ui/input/TextInput.js
@@ -1,38 +1,64 @@
-import * as React from 'react';
-import { TextInput as InternalTextInput } from 'react-native-paper';
-import { colors } from '../styles/colors';
-import { makeValidator, useValidator } from './useValidator';
-import { useThemeMaker } from '../styles/Color';
+import * as React from "react";
+import { TextInput as InternalTextInput } from "react-native-paper";
+import { colors } from "../styles/colors";
+import { makeValidator, useValidator } from "./useValidator";
+import { useThemeMaker } from "../styles/Color";
+import { margin } from "../styles/margin";
+import { Text } from "react-native";
 
 export default function TextInput({
-    color=colors.primary,
-    value = "",
-    label = undefined,
-    placeholder = undefined, //used if you only want a placeholder
-    onChange = undefined,
-    delay = undefined,
-    mode = "outlined",
-    password = undefined,
-    status = undefined,
-    style = {},
-    validator = makeValidator(status),
+  color = colors.primary,
+  value = "",
+  label = undefined,
+  outlineStyle = undefined,
+  outlineColor = undefined,
+  placeholder = undefined, //used if you only want a placeholder
+  placeholderTextColor = undefined,
+  onChange = undefined,
+  delay = undefined,
+  mode = "outlined",
+  password = undefined,
+  status = undefined,
+  keyboardType = "numeric",
+  style = {},
+  errorMsg = undefined,
+  validator = makeValidator(status),
 }) {
-    const validatorStatus = useValidator(validator, value, delay);
-    const theme = useThemeMaker({primary: color, status: status || validatorStatus})
+  const validatorStatus = useValidator(validator, value, delay);
+  const theme = useThemeMaker({
+    primary: color,
+    status: status || validatorStatus,
+  });
 
-    return(
-        <InternalTextInput
-            theme={theme}
-            secureTextEntry={!!password}
-            mode={mode}
-            placeholder={placeholder}
-            label={label}
-            value={value}
-            style={{
-                width:"100%",
-                ...style
-            }}
-            onChangeText={text => onChange(text)}
-        />
-    );
+  return (
+    <>
+      <InternalTextInput
+        theme={theme}
+        secureTextEntry={!!password}
+        mode={mode}
+        placeholder={placeholder}
+        label={label}
+        outlineStyle={{ ...outlineStyle }}
+        outlineColor={outlineColor}
+        placeholderTextColor={placeholderTextColor}
+        value={value}
+        keyboardType={keyboardType}
+        style={{
+          width: "100%",
+          ...style,
+        }}
+        onChangeText={(text) => onChange(text)}
+      />
+      <Text
+        style={{
+          marginLeft: margin.small,
+          marginTop: margin.small,
+          fontSize: 12,
+          color: colors.error.main,
+        }}
+      >
+        {errorMsg && validatorStatus == "error" ? errorMsg : ""}
+      </Text>
+    </>
+  );
 }
