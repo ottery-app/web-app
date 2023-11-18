6e9a146aa4f49056bde2620cab6fad26416a1491
lewibs <benjaminsl2000@gmail.com>
2023-11-17 23:07:12 -0500

    done #147

--- src/router/stacks/Header.js
+++ src/router/stacks/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Appbar, Menu, Text } from 'react-native-paper';
 import { ThemeProvider } from '../../../ottery-ui/styles/Color';
 import { logoDefault } from "../../../assets/logos";
 import Image from "../../../ottery-ui/image/Image";
 import { useAuthClient } from '../../features/auth/useAuthClient';
 import { role } from '@ottery/ottery-dto';
+import { colors } from '../../../ottery-ui/styles/colors';
 
 export default function Header({
   navigation,
@@ -16,14 +17,12 @@ export default function Header({
   const sesh = useAuthClient().useSesh();
   const logout = useAuthClient().useLogout();
 
-  console.log(sesh);
-
   return (
     <ThemeProvider>
-        <Appbar.Header style={{backgroundColor:"green"}}>
+        <Appbar.Header style={{backgroundColor:(sesh.state === role.CARETAKER)?colors.success.main:colors.primary.main}}>
             {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
             <Appbar.Content title={<Image src={logoDefault} width={150}/>} />
-            {(sesh.state !== role.CARETAKER)?<Text>Clocked in</Text>:undefined}
+            {(sesh.state === role.CARETAKER)?<Text>Clocked in</Text>:undefined}
             {!back ? (
                 <Menu
                     visible={visible}
