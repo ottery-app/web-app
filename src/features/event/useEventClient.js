7ba32a395b085d7f37a3bc9879226e61b48015ee
Paul <wmdev0808@gmail.com>
2023-11-27 18:09:38 +0800

    -- Create an utility function to get the event's lead manager

--- src/features/event/useEventClient.js
+++ src/features/event/useEventClient.js
@@ -1,75 +1,88 @@
-import { 
-    getEvents, 
-    newEvent,
-    signUpAttendeesByIds,
-    signUpVolenteersByIds,
-    getInfo,
-    getAttendeeSignup,
-    getVolenteerSignup,
-    getOwner,
+import {
+  getEvents,
+  newEvent,
+  signUpAttendeesByIds,
+  signUpVolenteersByIds,
+  getInfo,
+  getAttendeeSignup,
+  getVolenteerSignup,
+  getOwner,
 } from "./eventApi";
-import {makeUseMutation} from "../../queryStatus/makeUseMutation";
-import {makeUseQuery} from "../../queryStatus/makeGetQuery";
+import { makeUseMutation } from "../../queryStatus/makeUseMutation";
+import { makeUseQuery } from "../../queryStatus/makeGetQuery";
 
 export const QUERY_EVENT_TAG = "event";
 
 export function useEventClient() {
+  const useGetEventInfo = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "eventInfo"],
+    queryFn: getInfo,
+  });
 
-    const useGetEventInfo = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "eventInfo"],
-        queryFn: getInfo,
-    });
+  const useGetEvent = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "event"],
+    queryFn: async (event) => {
+      let res = await getEvents([event]);
+      res.data = res.data[0];
+      return res;
+    },
+  });
 
-    const useGetEvent = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "event"],
-        queryFn: async (event)=>{
-            let res = await getEvents([event]);
-            res.data = res.data[0];
-            return res;
-        }
-    });
+  const useGetEvents = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG],
+    queryFn: getEvents,
+  });
 
-    const useGetEvents = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG],
-        queryFn: getEvents,
-    });
+  const useGetEventOwner = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "owner"],
+    queryFn: getOwner,
+  });
 
-    const useGetEventOwner = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "owner"],
-        queryFn: getOwner,
-    })
+  const useGetAttendeeSignup = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "attendee", "signup"],
+    queryFn: getAttendeeSignup,
+  });
 
-    const useGetAttendeeSignup = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "attendee", "signup"],
-        queryFn: getAttendeeSignup,
-    })
+  const useGetVolenteerSignup = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "volenteer", "signup"],
+    queryFn: getVolenteerSignup,
+  });
 
-    const useGetVolenteerSignup = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "volenteer", "signup"],
-        queryFn: getVolenteerSignup,
-    })
+  const useNewEvent = makeUseMutation({
+    mutationFn: newEvent,
+  });
 
-    const useNewEvent = makeUseMutation({
-        mutationFn: newEvent,
-    });
+  const useSignUpAttendeesByIds = makeUseMutation({
+    mutationFn: signUpAttendeesByIds,
+  });
 
-    const useSignUpAttendeesByIds = makeUseMutation({
-        mutationFn: signUpAttendeesByIds,
-    })
+  const useSignUpVolenteersByIds = makeUseMutation({
+    mutationFn: signUpVolenteersByIds,
+  });
 
-    const useSignUpVolenteersByIds = makeUseMutation({
-        mutationFn: signUpVolenteersByIds,
-    })
+  function getLeadManager(eventId) {
+    const eventInfoQueryResult = useGetEventInfo({ inputs: eventId });
+    const eventInfo = eventInfoQueryResult?.data?.data;
 
-    return {
-        useGetEvent,
-        useGetEvents,
-        useNewEvent,
-        useGetEventInfo,
-        useSignUpAttendeesByIds,
-        useSignUpVolenteersByIds,
-        useGetAttendeeSignup,
-        useGetVolenteerSignup,
-        useGetEventOwner,
+    if (eventInfo.isSuccess) {
+      return eventInfo.leadManager;
     }
+    if (eventInfo.isError) {
+      return eventInfo.error;
+    }
+    return undefined;
+  }
+
+  return {
+    useGetEvent,
+    useGetEvents,
+    useNewEvent,
+    useGetEventInfo,
+    useSignUpAttendeesByIds,
+    useSignUpVolenteersByIds,
+    useGetAttendeeSignup,
+    useGetVolenteerSignup,
+    useGetEventOwner,
+    getLeadManager,
+  };
 }
