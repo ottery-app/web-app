b96c449af155c4d7917c8011c294154057e2e46c
Paul <wmdev0808@gmail.com>
2023-11-29 15:08:51 +0800

    -- Create ButtonField component

---
+++ ottery-ui/buttons/ButtonField.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { StyleSheet, View } from "react-native";
+import { margin } from "../styles/margin";
+
+export interface ButtonFieldProps {
+  children: ReactNode;
+}
+
+function ButtonField({ children }: ButtonFieldProps) {
+  return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: margin.small,
+    width: "100%",
+  },
+});
+
+export default ButtonField;
