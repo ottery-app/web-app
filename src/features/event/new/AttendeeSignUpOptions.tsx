678dafae3ec4c90ddfb668e3905c31e9e25ec8cb
Paul <wmdev0808@gmail.com>
2023-12-13 16:27:55 +0800

    -- Create a step screen for attendees' signup options

--- src/features/event/new/AttendeeSignUpOptions.tsx
+++ src/features/event/new/AttendeeSignUpOptions.tsx
@@ -1,7 +1,25 @@
-import { View } from "react-native";
+import HelpWrapper from "./components/UI/HelpWrapper";
+import { StepProps } from "../../../../ottery-ui/forms/MultiStepForm";
+import { EventFormData } from ".";
+import SignUpOptions, { makeHandleDone } from "./components/SignUpOptions";
+import Main from "./components/UI/Main";
 
-function AttendeeSignUpOptionsForm() {
-  return <View>Attendee sign up options</View>;
+function AttendeeSignUpOptionsForm({
+  form,
+  setForm,
+  updateErrorHandler,
+}: StepProps<EventFormData>) {
+  return (
+    <Main>
+      <HelpWrapper title="Attendee Info">
+        <SignUpOptions
+          fields={form.attendeeSignUp}
+          handleUpdate={makeHandleDone(setForm, "attendeeSignUp")}
+          updateErrorHandler={updateErrorHandler}
+        />
+      </HelpWrapper>
+    </Main>
+  );
 }
 
 export default AttendeeSignUpOptionsForm;
