00fafdcf30720b3cd6fb1d85bf736c14fc9ed168
lewibs <benjaminsl2000@gmail.com>
2023-12-12 20:15:04 -0500

    fixed icon button

--- assets/icons.ts
+++ assets/icons.ts
@@ -1,12 +1,12 @@
 import { ImageAsset } from "./ImageAsset";
 
 export const send: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/send.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/send.png",
   aspectRatio: 1,
 }
 
 export const info: ImageAsset = {
-  src: "https://github.com/ottery-app/global-data/blob/main/images/icons/info.svg?raw=true",
+  src: "https://github.com/ottery-app/global-data/blob/main/images/icons/info.png?raw=true",
   aspectRatio: 1,
 }
 
@@ -16,12 +16,12 @@ export const share: ImageAsset = {
 }
 
 export const happyCheck: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/26e52da8b8819343f465cfed5e66f0fa1b3e9fa2/images/icons/green_check.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/26e52da8b8819343f465cfed5e66f0fa1b3e9fa2/images/icons/green_check.png",
   aspectRatio: 1,
 }
 
 export const unhappyCheck: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/26e52da8b8819343f465cfed5e66f0fa1b3e9fa2/images/icons/red_x.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/26e52da8b8819343f465cfed5e66f0fa1b3e9fa2/images/icons/red_x.png",
   aspectRatio: 1,
 }
 
@@ -31,41 +31,41 @@ export const closedMailWithHalo: ImageAsset = {
 };
 
 export const pfp: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/pfp.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/pfp.png",
   aspectRatio: 1,
 };
 
 export const x: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/x.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/x.png",
   aspectRatio: 1,
 };
 
 export const check: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/check.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/check.png",
   aspectRatio: 1,
 };
 
 export const message: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/message.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/message.png",
   aspectRatio: 1,
 };
 
 export const pluss: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/plus.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/plus.png",
   aspectRatio: 1,
 };
 
 export const users: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/users.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/users.png",
   aspectRatio: 1,
 };
 
 export const clock: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/clock.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/clock.png",
   aspectRatio: 1,
 };
 
 export const alertMessage: ImageAsset = {
-  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/alertMessage.svg",
+  src: "https://raw.githubusercontent.com/ottery-app/global-data/main/images/icons/alertMessage.png",
   aspectRatio: 1,
 };
