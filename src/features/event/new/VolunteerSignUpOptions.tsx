906fc49d19ff7614e893ab4a82f879e8b1b48bdb
Paul <wmdev0808@gmail.com>
2023-12-13 16:00:59 +0800

    -- Create a step page for volunteer signup options

--- src/features/event/new/VolunteerSignUpOptions.tsx
+++ src/features/event/new/VolunteerSignUpOptions.tsx
@@ -1,7 +1,25 @@
-import { View } from "react-native";
+import HelpWrapper from "./components/UI/HelpWrapper";
+import { StepProps } from "../../../../ottery-ui/forms/MultiStepForm";
+import { EventFormData } from ".";
+import SignUpOptions, { makeHandleDone } from "./components/SignUpOptions";
+import Main from "./components/UI/Main";
 
-function VolunteerSignUpOptionsForm() {
-  return <View>Volunteer sign up options</View>;
+function VolunteerSignUpOptionsForm({
+  form,
+  setForm,
+  updateErrorHandler,
+}: StepProps<EventFormData>) {
+  return (
+    <Main>
+      <HelpWrapper title="Volunteer Info">
+        <SignUpOptions
+          fields={form.volunteerSignUp}
+          handleUpdate={makeHandleDone(setForm, "volunteerSignUp")}
+          updateErrorHandler={updateErrorHandler}
+        />
+      </HelpWrapper>
+    </Main>
+  );
 }
 
 export default VolunteerSignUpOptionsForm;
