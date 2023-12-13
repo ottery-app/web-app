b0f1deed6d93a2f67ae5ffe528e9ced3c3fb0100
Paul <wmdev0808@gmail.com>
2023-12-13 17:44:24 +0800

    -- Invalidate user event query when creating a new event

--- src/features/event/useEventClient.js
+++ src/features/event/useEventClient.js
@@ -1,76 +1,81 @@
-import { 
-    getEvents, 
-    newEvent,
-    signUpAttendeesByIds,
-    signUpVolenteersByIds,
-    getInfo,
-    getAttendeeSignup,
-    getVolenteerSignup,
-    getOwner,
-    signupUser,
+import {
+  getEvents,
+  newEvent,
+  signUpAttendeesByIds,
+  signUpVolenteersByIds,
+  getInfo,
+  getAttendeeSignup,
+  getVolenteerSignup,
+  getOwner,
+  signupUser,
 } from "./eventApi";
-import {makeUseMutation} from "../../queryStatus/makeUseMutation";
-import {makeUseQuery} from "../../queryStatus/makeGetQuery";
+import { makeUseMutation } from "../../queryStatus/makeUseMutation";
+import { makeUseQuery } from "../../queryStatus/makeGetQuery";
+import { queryClient } from "../../provider/queryClient";
+import { CLIENT_USER_TAG } from "../user/useUserClient";
 
 export const QUERY_EVENT_TAG = "event";
 
 export function useEventClient() {
+  const useGetEventInfo = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "eventInfo"],
+    queryFn: getInfo,
+  });
 
-    const useGetEventInfo = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "eventInfo"],
-        queryFn: getInfo,
-    });
+  const useGetEvent = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "event"],
+    queryFn: async (event) => {
+      let res = await getEvents([event]);
+      res.data = res.data[0];
+      return res;
+    },
+  });
 
-    const useGetEvent = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "event"],
-        queryFn: async (event)=>{
-            let res = await getEvents([event]);
-            res.data = res.data[0];
-            return res;
-        }
-    });
+  const useGetEvents = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG],
+    queryFn: getEvents,
+  });
 
-    const useGetEvents = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG],
-        queryFn: getEvents,
-    });
+  const useGetEventOwner = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "owner"],
+    queryFn: getOwner,
+  });
 
-    const useGetEventOwner = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "owner"],
-        queryFn: getOwner,
-    })
+  const useGetAttendeeSignup = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "attendee", "signup"],
+    queryFn: getAttendeeSignup,
+  });
 
-    const useGetAttendeeSignup = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "attendee", "signup"],
-        queryFn: getAttendeeSignup,
-    })
+  const useGetVolenteerSignup = makeUseQuery({
+    queryKey: [QUERY_EVENT_TAG, "volenteer", "signup"],
+    queryFn: getVolenteerSignup,
+  });
 
-    const useGetVolenteerSignup = makeUseQuery({
-        queryKey: [QUERY_EVENT_TAG, "volenteer", "signup"],
-        queryFn: getVolenteerSignup,
-    })
+  const useNewEvent = makeUseMutation({
+    mutationFn: newEvent,
+    onSuccessAlways: (data) => {
+      queryClient.invalidateQueries([CLIENT_USER_TAG, "events"]);
+      return data;
+    },
+  });
 
-    const useNewEvent = makeUseMutation({
-        mutationFn: newEvent,
-    });
+  const useSignUpAttendeesByIds = makeUseMutation({
+    mutationFn: signUpAttendeesByIds,
+  });
 
-    const useSignUpAttendeesByIds = makeUseMutation({
-        mutationFn: signUpAttendeesByIds,
-    })
+  const useSignupUser = makeUseMutation({
+    mutationFn: signupUser,
+  });
 
-    const useSignupUser = makeUseMutation({
-        mutationFn: signupUser,
-    })
-
-    return {
-        useGetEvent,
-        useGetEvents,
-        useNewEvent,
-        useGetEventInfo,
-        useSignUpAttendeesByIds,
-        useSignupUser,
-        useGetAttendeeSignup,
-        useGetVolenteerSignup,
-        useGetEventOwner,
-    }
+  return {
+    useGetEvent,
+    useGetEvents,
+    useNewEvent,
+    useGetEventInfo,
+    useSignUpAttendeesByIds,
+    useSignupUser,
+    useGetAttendeeSignup,
+    useGetVolenteerSignup,
+    useGetEventOwner,
+  };
 }
