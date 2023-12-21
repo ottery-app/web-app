d6f35f1a93668bd3928e204cec367154022c21c8
lewibs <benjaminsl2000@gmail.com>
2023-12-20 23:49:35 -0500

    fixed #336

--- ottery-ui/lists/AppendList.tsx
+++ ottery-ui/lists/AppendList.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View } from "react-native";
-
 import { PropsWithChildren, ReactElement } from "react";
 import { IconButton, Text } from "react-native-paper";
 import { colors } from "../styles/colors";
@@ -33,13 +32,15 @@ function Item({ children, id, onDelete }: PropsWithChildren<ItemProps>) {
   return (
     <View style={styles.itemField}>
       <View style={styles.item}>{children}</View>
-      <IconButton
-        icon="delete"
-        iconColor={colors.error.contrastText}
-        containerColor={colors.error.main}
-        onPress={handleDeleteItem(id)}
-        style={{ borderRadius: radius.default }}
-      />
+      <View style={styles.itemButton}>
+        <IconButton
+          icon="delete"
+          iconColor={colors.error.contrastText}
+          containerColor={colors.error.main}
+          onPress={handleDeleteItem(id)}
+          style={{ borderRadius: radius.default }}
+        />
+      </View>
     </View>
   );
 }
@@ -75,12 +76,14 @@ function AppendList<T>({
 const styles = StyleSheet.create({
   itemField: {
     flexDirection: "row",
-    alignItems: "baseline",
     justifyContent: "space-between",
   },
   item: {
-    flex: 1,
+    flex:6,
   },
+  itemButton: {
+    flex:1,
+  }
 });
 
 export default AppendList;
