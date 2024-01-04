8af22a11ecc21b88ea7b372847cd0355827f5001
lewibs <benjaminsl2000@gmail.com>
2024-01-03 20:48:44 -0500

    fixed issues in custom event time

--- src/features/event/new/components/CustomRepeat.tsx
+++ src/features/event/new/components/CustomRepeat.tsx
@@ -100,14 +100,34 @@ interface CustomRepeatProps {
 }
 
 function CustomRepeat({ rrule, setRRule }: CustomRepeatProps) {
-  const [endKey, setEndKey] = useState(END_TYPES.Never);
-  const [endValues, setEndValues] = useState({});
+  const [endKey, setEndKey] = useState(()=>{
+    const rule = {...rrule.origOptions};
+
+    if (rule.count) {
+      return END_TYPES.After;
+    } else if (rule.until) {
+      return END_TYPES.On;
+    } else {
+      return END_TYPES.Never;
+    }
+  });
+  const [endValues, setEndValues] = useState(()=>{
+    const rule = {...rrule.origOptions};
+    const endValues = {};
+    endValues[END_TYPES.Never] = undefined;
+    endValues[END_TYPES.After] = rule.count;
+    endValues[END_TYPES.On] = rule["until"];
+    return endValues;
+  });
   const [rule, setRule] = useState({...rrule.origOptions});
+
+  console.log(endValues);
 
   useEffect(()=>{
     if (rule.interval !== undefined) {
-      console.log(rule);
-      setRRule(new RRule(rule));
+      const rrule = new RRule(rule);
+      console.log(rrule.toString());
+      setRRule(rrule);
     }
   }, [rule]);
 
@@ -174,7 +194,7 @@ function CustomRepeat({ rrule, setRRule }: CustomRepeatProps) {
   }
 
   return (
-    <Main>
+    <View style={{gap:margin.large}}>
       <Head style={{ textAlign: "left" }}>Custom repeat</Head>
       <Row>
         <Text style={{ flex: 1 }}>Every: </Text>
@@ -221,7 +241,7 @@ function CustomRepeat({ rrule, setRRule }: CustomRepeatProps) {
           values={endValues}
         />
       </Row>
-    </Main>
+    </View>
   );
 }
 
